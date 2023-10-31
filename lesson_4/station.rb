34cec6ae3bec6492f04c4063b5ef0c56d6da9eb4
rav <reznichenkoav@me.com>
2023-10-31 10:29:03 +0300

    Fix Station.rb method 'trains_by_type'

--- lesson_4/station.rb
+++ lesson_4/station.rb
@@ -13,14 +13,8 @@ class Station
   def departure_train(train)
     @trains.delete(train)
   end
-
-  def trains_by_type(type) # to do
-    count = 0
-    @trains.each do |train|
-      if train.type == type
-        count += 1     
-      end
-    end
-    return "На станции #{type}: #{count}"
+  
+  def trains_by_type
+    @trains.each_with_object(Hash.new(0)) { |t, hh| hh[t.type] += 1 }
   end
 end

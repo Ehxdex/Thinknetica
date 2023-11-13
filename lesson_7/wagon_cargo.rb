65910607ad59344989f84471aaf25d3d9cc297ec
rav <reznichenkoav@me.com>
2023-11-13 16:17:45 +0300

    Add seats and volume to wagons

--- lesson_7/wagon_cargo.rb
+++ lesson_7/wagon_cargo.rb
@@ -1,5 +1,30 @@
 class WagonCargo < Wagon
-  def initialize
+  def initialize(volume)
+    @volume = volume
+    @filled = 0
+    validate!
     super(:cargo)
+  end
+
+  def fill_volume(fill)
+    free_volume - fill < 0 ? "Недостаточно объема" : @filled += fill
+  end
+
+  def used_volume
+    @filled
+  end
+
+  def free_volume
+    @volume - @filled
+  end
+
+  protected
+
+  def validate!
+    errors = []
+    errors << "Wagons volume can't be < 0" if @volume < 0
+    errors << "Wagons volume can't be nil" if @volume.nil?
+    errors << "Used volume can't be more than wagons volume" if @used_volume > @volume
+    raise ArgumentErrors, errors.join(', ') unless errors.empty?) 
   end
 end
--- lesson_7/wagon_passenger.rb
+++ lesson_7/wagon_passenger.rb
@@ -1,5 +1,28 @@
 class WagonPassenger < Wagon
-  def initialize
+  attr_reader :used_seats
+
+  def initialize(seats)
+    @seats = seats
+    @used_seats = 0
+    validate!
     super(:passenger)
+  end
+
+  def fill_wagon
+    @used_seats + 1 < @seats ? "Недостаточно мест" : @used_seats += 1
+  end
+
+  def free_seats
+    @seats - @used_seats
+  end
+
+  protected
+
+  def validate!
+    errors = []
+    errors << "Wagons seats can't be < 0" if @volume < 0
+    errors << "Wagons seats can't be nil" if @volume.nil?
+    errors << "Used seats can't be more than wagons seats" if @used_seats > @seats
+    raise ArgumentErrors, errors.join(', ') unless errors.empty?) 
   end
 end
